b8988f423557be68adee08dd8ae0853c9998811d
yjs <wptmd97@naver.com>
2023-01-12 13:52:48 +0900

    0112.3

---
+++ 백준/11651.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct where_xy{
+    int xx;
+    int yy;
+};
+
+bool cmp(const where_xy &p1, const where_xy &p2){
+    if(p1.yy < p2.yy){
+        return true;
+    }
+    else if(p1.yy == p2.yy){
+        return p1.xx < p2.xx;
+    }
+    else{
+        return false;
+    }
+}
+
+int main()
+{
+    int a;
+    cin>>a;
+    where_xy able[100000];
+    for(int x=0;x<a;x++)
+    {
+        cin>>able[x].xx>>able[x].yy;
+    }
+    sort(able,able+a,cmp);
+    
+    for(int x=0;x<a;x++)
+    {
+        cout<<able[x].xx<<" "<<able[x].yy<<"\n";
+    }
+    
+    
+    
+    return 0;
+}
+
