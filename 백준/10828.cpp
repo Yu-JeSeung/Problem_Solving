d77831db345994222ab2996eafab11d2f54dd6e3
yjs <wptmd97@naver.com>
2023-02-02 12:58:52 +0900

    0202.1

---
+++ 백준/10828.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+vector<string>v;
+string str;
+vector<int>i;
+
+void pop()
+{
+    if(v.size()==0)i.push_back(-1);
+    else {
+        i.push_back(stoi(v.back()));
+        v.pop_back();
+    }
+}
+
+void empty()
+{
+    int value=(int)v.size();
+    if(value!=0)i.push_back(0);
+    else i.push_back(1);
+}
+
+void top()
+{
+    if(v.size()==0)i.push_back(-1);
+    else {
+        i.push_back(stoi(v.back()));
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a;
+    cin>>a;
+    for(int x=0;x<a;x++)
+    {
+        string push_v;
+        cin>>str;
+        
+        if(str=="push"){
+            cin>>push_v;
+            v.push_back(push_v);
+        }
+        if(str=="pop"){
+            pop();
+        }
+        if(str=="size"){
+            i.push_back((int)v.size());
+        }
+        if(str=="empty"){
+            empty();
+        }
+        if(str=="top"){
+            top();
+        }
+    }
+   
+    for(int x=0;x<i.size();x++)
+    {
+        cout<<i[x]<<endl;
+    }
+    
+    return 0;
+}
