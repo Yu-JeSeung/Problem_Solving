29608308e47f26fb4a5cf34fe89d7e83bdf8a9bc
Yu-JeSeung <88806404+Yu-JeSeung@users.noreply.github.com>
2026-01-30 16:27:42 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 34900 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+n,k = map(int, input().strip().split(' '))
+result = []
+q = deque(range(1, n+1))
+
+for _ in range(n):
+  q.rotate(-(k-1))
+  result.append(q.popleft())
+
+print("<" + ", ".join(map(str, result)) + ">")
